a6572f6ff8a6be2f886bac49c1e2dbdc9b8ffd6c
AnyheloxD <aojeda.sullon@gmail.com>
2020-12-09 17:15:57 -0500

    registrousaurio.php

--- registrousuario.php
+++ registrousuario.php
@@ -16,24 +16,24 @@
 		
 		<form>
 			<div class="row flex-center">
-				<div class="col-11">
-					<input class="form-control campo1  f1" type="text" id="usuario" name="usuario" placeholder="Nombre de Usuario">
+				<div class="col-12  pl-4 pr-2">
+					<input class="form-control campo1  w-100 f1" type="text" id="usuario" name="usuario" placeholder="Nombre de Usuario">
 				</div>
 
-				<div class="col-11 mt-3">
-					<input class="form-control campo1  f1" type="text" id="correo" name="correo" placeholder="Correo Electronico">
+				<div class="col-12  pl-4 pr-2 mt-3">
+					<input class="form-control w-100 campo1  f1" type="text" id="correo" name="correo" placeholder="Correo Electronico">
 				</div>
-				<div class="col-11 mt-3">
+				<div class="col-12  pl-4 pr-2 mt-3">
 					
-					<input class="form-control f1 campo1" type="password" id="contraseña1" name="contraseña1" placeholder="Contraseña">
+					<input class="form-control w-100 f1 campo1" type="password" id="contraseña1" name="contraseña1" placeholder="Contraseña">
 				
 				</div>
-				<div class="col-11 mt-3">
+				<div class="col-12  pl-4 pr-2 mt-3">
 					
-					<input class="form-control f1 campo1" type="password" id="confirmarcontraseña" name="confirmarcontraseña" placeholder="Confirmar contraseña">
+					<input class="form-control w-100 f1 campo1" type="password" id="confirmarcontraseña" name="confirmarcontraseña" placeholder="Confirmar contraseña">
 				
 				</div>
-				<div class="col-11 mt-3">
+				<div class="col-12  pl-4 pr-2 mt-3">
 				
 <div class="custom-control custom-checkbox mt-4 mb-4">
   <input type="checkbox" class="custom-control-input" id="recuerdameusuario">
@@ -49,8 +49,8 @@
 			<div class="mt-4 mb-5">
 			<p class="text-center font-weight-bold f1">¿Ya tienes una cuenta? <a href="iniciodesesion.php" class="color1 ml-1">Inicia Sesión</a></p>
 		</div>
-		<div class="fixed-bottom text-center mt-5">
-			<p class="grey-text f3">@Kineshub 2020</p>
+		<div class="py-2 text-center mt-5">
+			<p class="grey-text f3">@Kineshub 2020. <br> Todos los derechos reservados.</p>
 		</div>
 	</main>
 
