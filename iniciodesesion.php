59578f5798b4c3ffbc8316b9add29391d9a7bcec
armando19993 <58012193+armando19993@users.noreply.github.com>
2020-12-09 20:11:28 -0500

    Iniio de sesion movil

--- iniciodesesion.php
+++ iniciodesesion.php
@@ -1,7 +1,9 @@
 <?php 
 	include "include/header.php";
-	
+	$url_api="http://localhost/api/";
  ?>
+ <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js" integrity="sha512-VEd+nq25CkR676O+pLBnDW09R7VQX9Mdiij052gVCp5yVH3jGtH70Ho/UUv4mJDsEdTvqRCFZg0NKGiojGnUCw==" crossorigin="anonymous"></script>
+
  <style>
  	header , footer {
  		display: none;
@@ -12,16 +14,18 @@
 		<div class="text-center my-5">
 			<a href="index.php"><img src="img/logo.png"  width="237" alt=""></a>
 			<h2 class="font-weight-bold f2 ">Inicio de sesión</h2>
+			<div class="col-12" id="mensaje"></div>
 		</div>
-		<form>
+		<form id="frm_login">
 			<div class="row flex-center">
 				<div class="col-11">
-					<input class="form-control campo1  f1" type="text" id="correo" name="correo" placeholder="Correo Electronico">
+					<input class="form-control campo1  f1" type="text" id="correoelectronico" name="usuario" placeholder="Correo Electronico">
+	 				<p style="color:red; font-size: 10px;" id="login_nombre"></p>
 				</div>
 				<div class="col-11 mt-3">
 					
-					<input class="form-control f1 campo1" type="password" id="contraseña" name="contraseña" placeholder="Contraseña">
-				
+					<input class="form-control f1 campo1" type="password" id="Contraseña" name="clave" placeholder="Contraseña">
+				<p style="color:red; font-size: 10px;" id="login_clave"></p>
 				</div>
 				<div class="col-11 mt-3">
 				
@@ -33,7 +37,7 @@
 			</div>
 			
 			<div class="text-center">
-				<a  class="btn boton1" type="submit">Ingresar</a>
+				<a class="btn boton1" onclick="login()">Ingresar</a>
 			</div>
 		</form>
 		<div class="mt-4">
@@ -49,7 +53,68 @@
 			<p class="grey-text f3">@Kineshub 2020. <br> Todos los derechos reservados.</p>
 		</div>
 	</main>
+<script>
+	function login(){
+    var nombre = $("#correoelectronico").val().length;
+    var clave = $("#Contraseña").val().length;
+    userdevdata = 2;
+    if(nombre == ''){
+      $("#login_nombre").html("Introduzca usuario o correo valido");
+    }
+    else{ userdevdata = userdevdata-1; console.log(userdevdata) }
+    if(clave == ''){
+      $("#login_clave").html("Introduzca una contraseña");
+    }
+    else{ userdevdata = userdevdata-1; console.log(userdevdata) }
 
+    if(userdevdata == 0){
+      var parametros=$( "#frm_login" ).serialize();
+                       $.ajax({
+                              data:  parametros, 
+                              url:   '<?= $url_api ?>public/json/login_user.php', 
+                              type:  'POST',
+                              success:  function (response) 
+                                          {
+                                            if (response==1) 
+                                            {
+                                              $("#login_nombre").html("");
+                                              $("#login_clave").html("");
+                                              $("#mensaje").html("<div class='alert alert-danger' role='alert'>Su cuenta aun no se encuentra validada, revise su correo o contacte con soporte!</div>")  
+                                            }
+                                            if (response==2) 
+                                            {
+                                              $("#login_nombre").html("");
+                                              $("#login_clave").html("");
+                                              $("#mensaje").html("<div class='alert alert-danger' role='alert'>Ha solicitado un cambio de contraseña, revise su correo y estara el link o vuelva a solicitar para restablecer su contraseña!</div>")  
+                                            }
+
+                                            if (response==3) 
+                                            {
+                                              $("#login_nombre").html("");
+                                              $("#login_clave").html("");
+                                              $("#mensaje").html("<div class='alert alert-danger' role='alert'>Su cuenta se encuentra suspendida, contacte con soporte!</div>")  
+                                            }
+
+                                            if (response==4) 
+                                            {
+                                              swal("Inicio de sesion correcto, redirigiendo.");  
+                                              url = "http://localhost/kineshub/";
+                                              setTimeout("location.href='http://localhost/kineshub/'", 5000);
+                                            }
+
+                                            if (response==5) 
+                                            {
+                                              $("#login_nombre").html("");
+                                              $("#login_clave").html("");
+                                              $("#mensaje").html("<div class='alert alert-danger' role='alert'>Usuario o Contraseña erroneos!</div>")  
+                                            }
+                                           
+                                          }
+                              
+                          });
+    }
+  }
+</script>
 	<?php 
 	include "include/footer.php";
 	include "modal.php";
