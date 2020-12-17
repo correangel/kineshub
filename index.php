56f4f0cbcc1cec04076a6dd87d6f3106c187c4da
armando19993 <58012193+armando19993@users.noreply.github.com>
2020-12-17 18:24:04 -0500

    Ajustes finales

--- index.php
+++ index.php
@@ -9,6 +9,10 @@ if(isset($_SESSION['id'])){
 else{
 	include "include/header.php";
 }
+
+$sql = mysqli_query($enlace, "SELECT anuncio.id AS ID, anuncio.verificado AS Verificado, anuncio.nombre AS Nombre, anuncio.edad AS Edad, anuncio.distrito, anuncio.provincia, anuncio.departamento, distritos.distrito AS Distrito, anuncio.pais AS Pais FROM anuncio INNER JOIN distritos ON anuncio.distrito = distritos.id WHERE estado = 1 ORDER BY orden DESC LIMIT 0, 30");
+	if(!$sql){echo "error".mysqli_error($enlace);}
+$num = mysqli_num_rows($sql);
  ?>
 <style>
 	body{
@@ -88,7 +92,67 @@ else{
 	
 	<div>
 	<div class="row mt-4" id="load_data">
-	
+	<?php while($row = mysqli_fetch_array($sql)){
+		$sqlq = mysqli_query($enlace, "SELECT * FROM imagenes WHERE id_anuncio = '". $row['ID'] ."' LIMIT 1");
+		$rowq = mysqli_fetch_array($sqlq);
+		$sqlp = mysqli_query($enlace, "SELECT min(costo) AS PrecioMinimo FROM tarifas WHERE id_anuncio = '". $row['ID']."'");
+		$rowp = mysqli_fetch_array($sqlp);
+		if(!$sqlp){
+			echo "error";
+		}
+		?>
+			
+			<div class="col-lg-2 col-6 px-1 mx-0">
+			<div class="card card1" >
+				<img src="images/<?= $rowq['imagen'] ?>" class="imagen1x" alt="" onclick="mostrar_modal('<?= $row['ID'] ?>')">
+				<div class="card-body">
+					<div class="row ">
+						<div class="col-8">
+							<div class="row">
+	<div class="col-12 " onclick="mostrar_modal('<?= $row['ID'] ?>')"><p><?= $row['Nombre'] ?> <?php if($row['Verificado'] == 1){  ?> <img src="img/Grupo 139.svg" alt="" class="ml-1"><?php } ?></p></div>
+								
+							</div>
+						</div>
+						<div class="col-4">
+
+						<?php if(isset($_SESSION['id']) && $_SESSION['tipo'] == 1){
+							$id = $row['ID'];
+							$sqlllll = mysqli_query($enlace, "SELECT id FROM favoritos WHERE id_anuncio = '$id' AND id_usuario = '". $_SESSION['id'] ."'");
+							$nummmmm = mysqli_num_rows($sqlllll);
+							if($nummmmm > 0){
+							?>
+							<i class="fas fa-heart f2 color3"></i>
+							<?php } else{ ?>
+
+						<i class="fas fa-heart f2" onclick="agregar_favoritos('<?=  $id ?>')" id="id_<?=  $id ?>"></i>
+						<?php } }
+						elseif(isset($_SESSION['id'])){
+						 ?>
+						<i class="fas fa-heart f2"></i>
+						<?php }
+						else{?>
+							<i class="fas fa-heart f2" data-toggle="modal" data-target="#sesion"></i>
+						<?php }
+						?>
+
+						
+
+						</div>
+					</div>
+				<div onclick="mostrar_modal('<?= $row['ID'] ?>')">
+					<span class="badge badge-pill badge-light"><?= $row['Distrito'] ?></span>
+					<span class="badge badge-pill badge-light"><?= $row['Edad'] ?> años</span>
+					<span class="badge badge-pill badge-light">S/ <?= $rowp['PrecioMinimo'] ?></span>
+					<span class="badge badge-pill badge-light"><?= $row['Pais'] ?></span>
+				</div>
+				</div>
+			</div>
+
+			</div>
+
+		
+			
+	<?php } ?>
 	</div>
 
 	<div class="row mt-4" id="load_data_message"></div>
@@ -316,7 +380,7 @@ function agregar_favoritos(anuncio){
 $(document).ready(function(){
  
  var limit = 30;
- var start = 0;
+ var start = 31;
  var action = 'inactive';
  function load_country_data(limit, start)
  {
@@ -330,7 +394,7 @@ $(document).ready(function(){
     $('#load_data').append(data);
     if(data == '')
     {
-     $('#load_data_message').html("<button type='button' class='btn btn-info'>No existen registros</button>");
+     $('#load_data_message').html("<button type='button' class='btn btn-info'>No hay mas anuncios</button>");
      action = 'active';
     }
     else
@@ -347,6 +411,7 @@ $(document).ready(function(){
   action = 'active';
   load_country_data(limit, start);
  }
+
  $(window).scroll(function(){
   if($(window).scrollTop() + $(window).height() > $("#load_data").height() && action == 'inactive')
   {
