5270954c60157f8253129f253669862778da1058
armando19993 <58012193+armando19993@users.noreply.github.com>
2020-12-16 19:23:50 -0500

    Muestreo

--- modal/perfil.php
+++ modal/perfil.php
@@ -6,43 +6,10 @@
 <div class="modal fade" id="perfilkine" tabindex="-1" role="dialog" aria-labelledby="perfilkine"
   aria-hidden="true">
   <div class="modal-dialog modal-dialog-centered  modal-lg" role="document">
-    <div class="modal-content">
-      <div class="d-block d-md-none">
-         <div class="text-left mr-5 ">
-         <button type="button" class="close cerrar2" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-           <img src="img/Rectangle 3.png" class="card-img-top" alt="">
-      </div>
-       <div class="text-left mr-5 d-none d-md-block">
-         <button type="button" class="close cerrar1" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body" id="contenido_modal">
-       
-      </div>
-      <div class="modal-footer py-0 my-0 d-block d-md-none">
+    <div class="modal-content" id="contenido_modal">
       
-            <div class="row pr-2">
-          <div class="col px-0 mx-0">
-            <button class="btn boton18 w-100 waves-effect waves-light" style="
-    font-size: 10px;
-"><img src="img/Icon ionic-ios-call.svg" width="15" alt="">  Llamar</button>
-          </div>
-          <div class="col px-2 mx-0">
-            <button class="boton17 w-100 btn waves-effect waves-light"><img src="img/Icon ionic-logo-whatsapp.svg" width="15" alt=""></button>
-          </div>
-          <div class="col px-0 mx-0">
-            <button class="btn  w-100 boton16 waves-effect waves-light" style="
-    height: 46px;
-    font-size: 10px;
-"><i class="fas fa-heart "></i> Favoritos</button>
-          </div>
-        </div>
-      </div>
-     
+          
+
     </div>
   </div>
 </div>
