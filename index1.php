5cb7cbfdfeaeb62299d08899fe047c9ebc5a7702
AnyheloxD <aojeda.sullon@gmail.com>
2020-12-18 14:35:25 -0500

    2:35 pm

--- index1.php
+++ index1.php
@@ -22,9 +22,74 @@ else{
   
       
  <div class="container-fluid ">
- <div class="row movilinicio"> 
+  <div class="d-block d-md-none">
+  <div class="container ">
+      <div class="input-group md-form form-sm form-1 pl-0">
+ 
+  <input class="form-control my-0 py-1 buscador1" type="text" autocomplete="off" placeholder="Buscar <?= $num ?> Anuncios" aria-label="Search">
+   <div class="input-group-prepend">
+    <span class="input-group-text text-dark" id="buscador" style="border-radius: 3px;"><i class="fas fa-search  pr-1"
+        aria-hidden="true"></i> Buscar</span>
+  </div>
+</div>
+  <div class="row">
+    <div class="col-6 pl-2 pr-2">
+      <button class="btn boton4 w-100 pt-2" data-toggle="modal" data-target="#ubicacion">Ubicación</button>
+    </div>
+    <div class="col-6 pl-2">
+      <button class="btn boton5 w-100 mx-0 pt-2" data-toggle="modal" data-target="#filtro">Filtros</button>
+    </div>
+  </div>
+
+
+    </div>
+  
+</div>
+    
+    <!-- fin movil  -->
+  <div class="d-none d-md-block">
+    <div class="row d-flex align-items-center ">
+    <div class="col-lg-6">
+      <div class="row d-flex justify-content-start">
+        <div class="col-lg-4 ">
+          <button class="btn text-dark boton8 font-weight-bold px-4"><i class="fas fa-tag pr-3"></i> Promociones</button>
+        </div>
+        <div class="col-lg-4  ">
+          <button class="btn boton9 text-white  font-weight-bold px-4" data-toggle="modal" data-target="#ubicacion"><i class="fas fa-map-marker-alt mr-3"></i> Ubicación</button>
+        </div>
+        <div class="col-lg-4 ">
+          <button class="btn boton10 text-white  font-weight-bold px-4"  data-toggle="modal" data-target="#filtro"><i class="fas fa-filter mr-3"></i> Filtros</button>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-6 ">
+      
+        <div class="row d-flex justify-content-end align-items-center">
+        <div class="col-lg-4"></div>
+        <div class="col-lg-4">
+        <?php if(isset($_SESSION['id']) && $_SESSION['tipo'] == 1){?>
+          <div class="fondo1">
+            <p class="pt-2 font-weight-bold text-center"><i class="fas fa-heart text-danger mr-3"></i> <a href="favoritos.php" class="text-dark">Favotiro <span class="ml-3">
+             <?php
+              $sqlll = mysqli_query($enlace, "SELECT id FROM favoritos WHERE id_usuario = '". $_SESSION['id'] ."'");
+              $nummm = mysqli_num_rows($sqlll);    
+              
+              echo $nummm;
+            ?>
+            </a></span></p>
+          </div>
+        <?php } ?>
+
+        </div>
+      
+      </div>
+      
+    </div>
+  </div>
+  </div>  </div>
+ <div class="row movilinicio mt-2"> 
    <div class="row mx-1 mx-lg-2 w-100">
-     <div class="card-columns">
+     <div class="card-columns" style="height: 100%;">
 <?php while($row = mysqli_fetch_array($sql)){
     $sqlq = mysqli_query($enlace, "SELECT * FROM imagenes WHERE id_anuncio = '". $row['ID'] ."' LIMIT 1");
     $rowq = mysqli_fetch_array($sqlq);
@@ -36,7 +101,7 @@ else{
     ?>
       <!-- Aqui -->
        <div class="card">
-           <img src="images/<?= $rowq['imagen'] ?>" class="imagen1x" alt="" onclick="mostrar_modal('<?= $row['ID'] ?>')">
+           <img src="images/<?= $rowq['imagen'] ?>" class="card-img-top" alt="" onclick="mostrar_modal('<?= $row['ID'] ?>')">
         <div class="card-body">
           <div class="row ">
             <div class="col-8">
