d0a89ab1d4c64ed04db097b8a2650b41c83e51fd
armando19993 <58012193+armando19993@users.noreply.github.com>
2020-12-22 05:26:57 -0500

    Update filtro_mayor.php

--- filtro_mayor.php
+++ filtro_mayor.php
@@ -213,19 +213,19 @@ $sql = "SELECT id_anuncio FROM caracteristicas WHERE id > 0";
                    
 				<!-- Aqui -->
                 <div class="card">
-           <img src="images/<?= $rowq['imagen'] ?>" class="card-img-top" alt="" onclick="mostrar_modal('<?= $row['ID'] ?>')">
+           <img src="images/<?= $rowq['imagen'] ?>" class="card-img-top" alt="" onclick="mostrar_modal('<?= $rowfinal['ID'] ?>')">
         <div class="card-body">
           <div class="row ">
             <div class="col-8">
               <div class="row">
-  <div class="col-12 " onclick="mostrar_modal('<?= $row['ID'] ?>')"><p><?= $row['Nombre'] ?> <?php if($row['Verificado'] == 1){  ?> <img src="img/Grupo 139.svg" alt="" class="ml-1"><?php } ?></p></div>
+  <div class="col-12 " onclick="mostrar_modal('<?= $rowfinal['ID'] ?>')"><p><?= $rowfinal['Nombre'] ?> <?php if($rowfinal['Verificado'] == 1){  ?> <img src="img/Grupo 139.svg" alt="" class="ml-1"><?php } ?></p></div>
                 
               </div>
             </div>
             <div class="col-4">
 
                        <?php if(isset($_SESSION['id']) && $_SESSION['tipo'] == 1){
-              $id = $row['ID'];
+              $id = $rowfinal['ID'];
               $sqlllll = mysqli_query($enlace, "SELECT id FROM favoritos WHERE id_anuncio = '$id' AND id_usuario = '". $_SESSION['id'] ."'");
               $nummmmm = mysqli_num_rows($sqlllll);
               if($nummmmm > 0){
@@ -247,11 +247,11 @@ $sql = "SELECT id_anuncio FROM caracteristicas WHERE id > 0";
 
             </div>
           </div>
-       <div onclick="mostrar_modal('<?= $row['ID'] ?>')">
-          <span class="badge badge-pill badge-light"><?= $row['Distrito'] ?></span>
-          <span class="badge badge-pill badge-light"><?= $row['Edad'] ?> años</span>
+       <div onclick="mostrar_modal('<?= $rowfinal['ID'] ?>')">
+          <span class="badge badge-pill badge-light"><?= $rowfinal['Distrito'] ?></span>
+          <span class="badge badge-pill badge-light"><?= $rowfinal['Edad'] ?> años</span>
           <span class="badge badge-pill badge-light">S/ <?= $rowp['PrecioMinimo'] ?></span>
-          <span class="badge badge-pill badge-light"><?= $row['Pais'] ?></span>
+          <span class="badge badge-pill badge-light"><?= $rowfinal['Pais'] ?></span>
         </div>
         </div>
        </div>
