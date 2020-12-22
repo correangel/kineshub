0ae5eb11822d5e3bc1aa2cdd56a2839ba50f650b
armando19993 <58012193+armando19993@users.noreply.github.com>
2020-12-22 01:43:12 -0500

    cc j

--- publicaranuncio.php
+++ publicaranuncio.php
@@ -797,7 +797,7 @@ border-color: #FF1730!important;
 	
 </main>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js" integrity="sha512-AA1Bzp5Q0K1KanKKmvN/4d3IRKVlv9PYgwFPvm32nPO6QS8yH1HO7LbgB1pgiOxPtfeg5zEn2ba64MUcqJx6CA==" crossorigin="anonymous"></script>
-
+<script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
 <script>
 	$(document).ready(function(){
     // File upload via Ajax
