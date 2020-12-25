05a864a38c0f87de236a1f37da854b7d0913b158
armando19993 <58012193+armando19993@users.noreply.github.com>
2020-12-25 18:41:26 -0500

    Update despurar.php

--- despurar.php
+++ despurar.php
@@ -4,10 +4,9 @@ include "db.php";
 $sql = mysqli_query()
 
 $thefolder = "images/";
-$i = 0;
 if ($handler = opendir($thefolder)) {
     while (false !== ($file = readdir($handler))) {
-            echo "$file ".$i++."<br>";
+            echo "$file<br>";
     }
     closedir($handler);
 }
