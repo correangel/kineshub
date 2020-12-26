0bac4312846b3f3b5c3c722448dc71573c01e6a0
armando19993 <58012193+armando19993@users.noreply.github.com>
2020-12-26 12:48:16 -0500

    Update favoritos2.php

--- favoritos2.php
+++ favoritos2.php
@@ -1,6 +1,17 @@
 <?php 
+session_start();
+require_once "db.php";
+mysqli_set_charset($enlace,"utf8");
+$sql = mysqli_query($enlace, "SELECT anuncio.id AS ID, anuncio.verificado AS Verificado, anuncio.nombre AS Nombre, anuncio.edad AS Edad, anuncio.distrito, anuncio.provincia, anuncio.departamento, distritos.distrito AS Distrito, anuncio.pais AS Pais FROM anuncio INNER JOIN distritos ON anuncio.distrito = distritos.id WHERE estado = 1");
+$num = mysqli_num_rows($sql);
+
+$sql_fav = mysqli_query($enlace, "SELECT * FROM favoritos WHERE id_usuario = '". $_SESSION['id'] ."'");
+if(isset($_SESSION['id'])){
+	include "include/header2.php";
+}
+else{
 	include "include/header.php";
-	
+}
  ?>
 
 <main>
@@ -41,38 +52,67 @@
 
 	<div class="row">
 		<div class="card-columns">
+			<?php while($row2 = mysqli_fetch_array($sql_fav)){
+        $sql = mysqli_query($enlace, "SELECT anuncio.id AS ID, anuncio.verificado AS Verificado, anuncio.nombre AS Nombre, anuncio.edad AS Edad, anuncio.distrito, anuncio.provincia, anuncio.departamento, distritos.distrito AS Distrito, anuncio.pais AS Pais FROM anuncio INNER JOIN distritos ON anuncio.distrito = distritos.id WHERE anuncio.id = '". $row2['id_anuncio'] ."'");
+        if(!$sql){echo "error".mysqli_error($enlace);}
+        $row = mysqli_fetch_array($sql);
+        $sqlq = mysqli_query($enlace, "SELECT * FROM imagenes WHERE id_anuncio = '". $row['ID'] ."' LIMIT 1");
+        if(!$sqlq){echo "error".mysqli_error($enlace);}
+		$rowq = mysqli_fetch_array($sqlq);
+		$sqlp = mysqli_query($enlace, "SELECT min(costo) AS PrecioMinimo FROM tarifas WHERE id_anuncio = '". $row['ID']."'");
+        if(!$sqlp){echo "error".mysqli_error($enlace);}
+        $rowp = mysqli_fetch_array($sqlp);
+		if(!$sqlp){
+			echo "error";
+		}
+		?>
+
 			<div class="card">
-				<img src="images/1607830046.jpeg" class="card-img-top" style="object-fit: cover;" alt="">
+				<img src="images/<?= $rowq['imagen'] ?>" class="imagen1x" alt="" onclick="mostrar_modal('<?= $row['ID'] ?>')">
 				<div class="card-body">
 					<div class="row ">
 						<div class="col-8">
 							<div class="row">
-	<div class="col-12 "><p>Vanesaa </p></div>
+	<div class="col-12 " onclick="mostrar_modal('<?= $row['ID'] ?>')"><p><?= $row['Nombre'] ?> <?php if($row['Verificado'] == 1){  ?> <img src="img/Grupo 139.svg" alt="" class="ml-1"><?php } ?></p></div>
 								
 							</div>
 						</div>
 						<div class="col-4">
 
-													<i class="fas fa-heart f2 color3"></i>
-							
+						<?php if(isset($_SESSION['id']) && $_SESSION['tipo'] == 1){
+							$id = $row['ID'];
+							$sqlllll = mysqli_query($enlace, "SELECT id FROM favoritos WHERE id_anuncio = '$id' AND id_usuario = '". $_SESSION['id'] ."'");
+							$nummmmm = mysqli_num_rows($sqlllll);
+							if($nummmmm > 0){
+							?>
+							<i class="fas fa-heart f2 color3"></i>
+							<?php } else{ ?>
+
+						<i class="fas fa-heart f2" onclick="agregar_favoritos('<?=  $id ?>')" id="id_<?=  $id ?>"></i>
+						<?php } }
+						elseif(isset($_SESSION['id'])){
+						 ?>
+						<i class="fas fa-heart f2"></i>
+						<?php }
+						else{?>
+							<i class="fas fa-heart f2" data-toggle="modal" data-target="#sesion"></i>
+						<?php }
+						?>
+
 						
 
 						</div>
 					</div>
-				<div onclick="mostrar_modal('Pmfm5tMGeupUsJHJt8IiXUT1LqeMYzY6kp38geGdSHbyyleMYi')">
-					<span class="badge badge-pill badge-light">SantaAnita</span>
-					<span class="badge badge-pill badge-light">24 años</span>
-					<span class="badge badge-pill badge-light">S/ 100</span>
-					<span class="badge badge-pill badge-light"></span>
+				<div onclick="mostrar_modal('<?= $row['ID'] ?>')">
+					<span class="badge badge-pill badge-light"><?= $row['Distrito'] ?></span>
+					<span class="badge badge-pill badge-light"><?= $row['Edad'] ?> años</span>
+					<span class="badge badge-pill badge-light">S/ <?= $rowp['PrecioMinimo'] ?></span>
+					<span class="badge badge-pill badge-light"><?= $row['Pais'] ?></span>
 				</div>
 				</div>
-				<div>
-					<button type="button" class="close cerrar1" style="position: absolute;top: 0;right: 0;" >
-          <span aria-hidden="true">×</span>
-        </button>
-				</div>
+				
 			</div>
-
+<?php } ?>
 		</div>
 	</div>
 	
