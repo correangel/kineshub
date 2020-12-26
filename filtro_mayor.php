33e2e9a2364aa41c028c0ea8b113a517b608c2d5
armando19993 <58012193+armando19993@users.noreply.github.com>
2020-12-25 23:33:09 -0500

    Update filtro_mayor.php

--- filtro_mayor.php
+++ filtro_mayor.php
@@ -108,7 +108,7 @@ include "consultas.php";
                 $resultve = mysqli_query($enlace, $sqlve);
                 $numve = mysqli_num_rows($resultve);
 
-                echo $sqlve;
+                
 
                 if($numve > 0){
                     
@@ -164,6 +164,8 @@ include "consultas.php";
 
                 $resultve = mysqli_query($enlace, $sqlve);
                 $numve = mysqli_num_rows($resultve);
+
+                echo $sqlve;
 
                 if($numve > 0){
                     $sqlfinal = mysqli_query($enlace, "SELECT anuncio.id AS ID, anuncio.verificado AS Verificado, anuncio.nombre AS Nombre, anuncio.edad AS Edad, anuncio.distrito, anuncio.provincia, anuncio.departamento, distritos.distrito AS Distrito, anuncio.pais AS Pais FROM anuncio INNER JOIN distritos ON anuncio.distrito = distritos.id WHERE anuncio.id = '$id'");
