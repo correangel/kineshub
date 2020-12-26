4283506e0c1023e74ceed2f5e3f08001044cef95
armando19993 <58012193+armando19993@users.noreply.github.com>
2020-12-25 19:21:13 -0500

    Update despurar.php

--- despurar.php
+++ despurar.php
@@ -1,12 +1,49 @@
 <?php
 include "db.php";
+/*$sql = mysqli_query($enlace, "SELECT * FROM anuncio");
+$anuncios = mysqli_num_rows($sql);
+$sql = mysqli_query($enlace, "SELECT * FROM anuncio WHERE estado = 1");
+$publicados = mysqli_num_rows($sql);
+$sql = mysqli_query($enlace, "SELECT * FROM anuncio WHERE estado = 0");
+$sin_publicar = mysqli_num_rows($sql);
+$i = 0;*/
 
-$sql = mysqli_query()
+
+//depurar Anuncios sin publicar
+if($sin_publicar > 0){
+	$i = 0;
+	$imaganes_depuradas = 0;
+	while ($row = mysqli_fetch_array($sql)) {
+		$id = $row['id'];
+		//$sql_f = mysqli_query("DELETE FROM anuncio WHERE estado = 0 AND id = '$id'");
+		if(!$sql_f){}
+			else{ 
+				$eliminados = $i++;
+				//$sqli = mysqli_query($enlace, "DELETE FROM imagenes WHERE id_anuncio = '$id'");
+				//$imaganes_depuradas = $imaganes_depuradas + mysqli_affected_rows($enlace); 
+			 }
+	}
+}
+
+echo "Anuncios Revisados: $anuncios <br>";
+echo "Anuncios Eliminados: $eliminados <br>";
+echo "Imagenes Depuradas DB: $imaganes_depuradas<br>";
+
 
 $thefolder = "images/";
 if ($handler = opendir($thefolder)) {
     while (false !== ($file = readdir($handler))) {
-            echo "$file<br>";
+        
+        if($file != '.' OR $file != '..' OR $file != '...'){
+        	$sql = mysqli_query($enlace, "SELECT * FROM imagenes WHERE imagen = '$file'");
+        	$num = mysqli_num_rows($sql);
+
+        	if($num > 0){echo "Archivo Existe";}
+        	else{
+        		echo "Archivo no existe";
+        		//unlink("images/".$file)?$eli++:"Error";
+        	}
+        }
     }
     closedir($handler);
 }
