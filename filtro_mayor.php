e0a4cbc663657c8040ed549e14cd2afa463309a8
armando19993 <58012193+armando19993@users.noreply.github.com>
2020-12-25 23:30:40 -0500

    Cuando le doy clic al botón verificar y panel de kine hay un error vid.2

--- filtro_mayor.php
+++ filtro_mayor.php
@@ -10,6 +10,7 @@ include "consultas.php";
                 echo "Aqui hay un error";
             }
             else{
+                echo "sql";
             }
 
 
@@ -59,6 +60,8 @@ include "consultas.php";
 
                 $resultve = mysqli_query($enlace, $sqlve);
                 $numve = mysqli_num_rows($resultve);
+
+                echo $sqlve;
 
                 if($numve > 0){
                     $sqlfinal = mysqli_query($enlace, "SELECT anuncio.id AS ID, anuncio.verificado AS Verificado, anuncio.nombre AS Nombre, anuncio.edad AS Edad, anuncio.distrito, anuncio.provincia, anuncio.departamento, distritos.distrito AS Distrito, anuncio.pais AS Pais FROM anuncio INNER JOIN distritos ON anuncio.distrito = distritos.id WHERE anuncio.id = '$id'");
